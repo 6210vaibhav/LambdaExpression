12005c32e9132226e240cb7e43e1bd4df0452b0f
6210vaibhav <81671189+6210vaibhav@users.noreply.github.com>
2021-05-16 23:30:11 +0530

    welcome to LamdaExpression

---
+++ bz/Lambda/MathOperationApp.java
@@ -0,0 +1,26 @@
+package com.bz.Lambda;
+
+@FunctionalInterface
+interface IMathFunction{
+	int calculate(int a, int b);
+}
+
+public class MathOperationApp {
+	
+	public static void main(String[] args) {
+		
+		IMathFunction add = Integer::sum;
+		
+		IMathFunction multiply = (x, y) -> x * y;
+		IMathFunction divide = (int x, int y) -> x / y;
+		
+		System.out.println("Addition is " + add.calculate(6, 3));
+		System.out.println("Multiplication is " + multiply.calculate(6, 3));
+		System.out.println("Division is " + divide.calculate(6, 3));
+		
+		
+		
+	}
+	
+
+}
