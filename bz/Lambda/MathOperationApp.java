1eaa16dfe7e84994173a7bf621a569050a2664eb
6210vaibhav <81671189+6210vaibhav@users.noreply.github.com>
2021-05-16 23:54:38 +0530

    Added UC1

--- bz/Lambda/MathOperationApp.java
+++ bz/Lambda/MathOperationApp.java
@@ -3,6 +3,9 @@ package com.bz.Lambda;
 @FunctionalInterface
 interface IMathFunction{
 	int calculate(int a, int b);
+	static void printresult(int a, int b, String function,IMathFunction fobj) {
+		System.out.println("Result of "+function+" is "+fobj.calculate(a, b));
+	}
 }
 
 public class MathOperationApp {
@@ -17,6 +20,10 @@ public class MathOperationApp {
 		System.out.println("Addition is " + add.calculate(6, 3));
 		System.out.println("Multiplication is " + multiply.calculate(6, 3));
 		System.out.println("Division is " + divide.calculate(6, 3));
+		
+		IMathFunction.printresult(6, 3, "Addition",add);
+		IMathFunction.printresult(6, 3, "Multiplication",multiply );
+		IMathFunction.printresult(6, 3, "Division",divide);
 		
 		
 		
